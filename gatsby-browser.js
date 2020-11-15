f35e9fc741529319a7a921bf06cd50a61ba59d32
LiXuanqi <lixuanqi1995@gmail.com>
2020-11-14 21:27:44 -0800

    [Config] remove useless hack config for css reset

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,33 +5,7 @@ import "typeface-merriweather"
 // Style for code
 import './src/css/prism-theme.scss'
 
-// import "react-toggle/style.css"
-// import "prism-theme-night-owl";
+import "react-toggle/style.css"
+import './src/css/index.css'
 
 require("prismjs/plugins/command-line/prism-command-line.css")
-export const onClientEntry = () => {
-    if (process.env.BUILD_STAGE === 'develop') {
-        // TODO: make the logic of css reset more clear.
-        const head = document.head;
-
-        const CSSReset = require('!postcss-loader!./src/css/index.css');
-        const styleNode = document.createElement('style')
-        styleNode.id = 'css-reset';
-        styleNode.innerHTML = CSSReset;
-
-        const ReactToggleCSS = require('!postcss-loader!react-toggle/style.css');
-        const ReactToggleStyleNode = document.createElement('style')
-        ReactToggleStyleNode.id = 'react-toggle-css';
-        ReactToggleStyleNode.innerHTML = ReactToggleCSS;
-
-        head.appendChild(styleNode);
-        head.insertBefore(ReactToggleStyleNode, styleNode);
-        // } else {
-        //     if (head.firstChild) {
-        //         head.insertBefore(styleNode, head.firstChild);
-        //     } else {
-        //         head.appendChild(styleNode);
-        //     }
-        // }
-    }
-}
